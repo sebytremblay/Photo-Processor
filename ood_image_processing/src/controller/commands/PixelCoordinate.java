ae8bb399d046c78d40bca369bf2237bd6126cb8f
sebytremblay <tremblay.se@northeastern.edu>
2022-11-29 19:34:52 -0500

    image mosiacking

---
+++ ood_image_processing/src/controller/commands/PixelCoordinate.java
@@ -0,0 +1,11 @@
+package controller.commands;
+
+public class PixelCoordinate {
+  public final int x;
+  public final int y;
+
+  PixelCoordinate(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+}
